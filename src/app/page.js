7f7da4ed9603683721857ba9e07e32b23bebc7da
ayasahmad63 <ayasahmad63@gmail.com>
2023-06-30 04:52:32 +0530

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full h-sc    bg-gradient-to-r  from-rgba-10-87-131-1  to-rgba-24-161-154-1">
+      <div className="h-2/3 flex justify-center items-center w-full  ">
+        <div className="w-2/3 p-3  pt-16 mt-4   text-center">
+          <h1 className="font-poppins text-6xl font-semibold leading-tight tracking-wider text-rgba-255-255-255-1">
+            Check your financial health
+          </h1>
+          <div className="font-poppins text-2xl   leading-42 tracking-normal text-center text-rgba-255-255-255-5">
+            Use WeathoMeter to get a free report
+            <br />
+            card for your finances- within minutes!
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex pt-10 -pb-16 justify-center items-center">
+        <button className="bg-rgba-251-115-6-1  text-2xl hover:bg-rgba-251-115-6-8 text-rgba-255-255-255-1 font-bold  py-2 px-4 rounded-full shadow-xl">
+          Get Started
+        </button>
       </div>
+      <div className="flex pt-10">
+        <div className="ml-32 pl-28 pt-10 ">
+          <div className="p1-1 flex ml-16 pl-16 pt-14">
+            <div className="flex">
+              <Image
+                src="/tick.svg"
+                alt="My Image"
+                className=""
+                width={27}
+                height={10}
+              />
+            </div>
+            <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1">
+              Expected Retirement Age
+            </div>
+          </div>
+          <div className="pl-16 ml-16">
+            <Image
+              src="/Line.svg"
+              alt="My Image"
+              className="py-3 ml-9"
+              width={290}
+              height={650}
+            />
+          </div>
+          <div className="pl-1 flex ml-16 pl-16 pt-4">
+            <div className="flex">
+              <Image
+                src="/tick.svg"
+                alt="My Image"
+                className=""
+                width={27}
+                height={10}
+              />
+            </div>
+            <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1">
+              Identify Mistake
+            </div>
+          </div>
+          <div className="pl-16 ml-16">
+            <Image
+              src="/Line.svg"
+              alt="My Image"
+              className="py-3 ml-9"
+              width={290}
+              height={650}
+            />
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex-1 -mr-28 -mb-28 -pr-28 g-green-500">
+          <div className="flex  justify-center items-center">
+            <div className="justify-center items-center">
+              <Image
+                src="/mobi.svg"
+                alt="My Image"
+                // className="-m-8 -mr-20"
+                width={480}
+                height={1500}
+              />
+            </div>
+          </div>{" "}
+        </div>
+        <div className="mr-32 pr-28 pt-10 z-10">
+          <div className="flex mr-1 px-1 pt-14 mr-16 pr-16">
+            <div className="flex ">
+              <Image
+                src="/tick.svg"
+                alt="My Image"
+                className=""
+                width={27}
+                height={10}
+              />
+            </div>
+            <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1">
+              Personalised Road Map
+            </div>
+          </div>
+          <div className="px-1 ml-1">
+            <Image
+              src="/Line.svg"
+              alt="My Image"
+              className="py-3 ml-9"
+              width={290}
+              height={950}
+            />
+          </div>
+          <div className="px-1 flex ml-1 px-1 pt-4">
+            <div className="flex">
+              <Image
+                src="/tick.svg"
+                alt="My Image"
+                className=""
+                width={27}
+                height={10}
+              />
+            </div>
+            <div className="flex px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1">
+              Tips To Improve
+            </div>
+          </div>
+          <div className="px-1 ml-1 mr-16 pr-16">
+            <Image
+              src="/Line.svg"
+              alt="My Image"
+              className="py-3 ml-9"
+              width={290}
+              height={650}
+            />
+          </div>
+        </div>
       </div>
-    </main>
-  )
+      <div
+        className=" bg-rd-500 relative bg-cover bg-center bg-no-repeat  -mt-20 -pt-22 absolut  h-screen bg-cover bg-center bg-gradient-to-r  from-rgba-10-87-131-1  to-rgba-24-161-154-1 "
+        style={{ backgroundImage: "url('/Vector1.svg')" }}
+      >
+        <div className="pt-28 flex justify-center items-center  ">
+          <div className="p-3  pt-16 mt-4   text-center">
+            <h1 className="font-poppins pt-16 text-6xl font-bold leading-tight tracking-wider text-rgba-255-255-255-1">
+              How it works?
+            </h1>{" "}
+            <div className="justify-center items-center w-full">
+              <Image
+                src="/wordks.svg"
+                alt="My Image"
+                className="-pt-16 pl-16 ml-12 -mb-28 -pb-28 -mb-28 -pt-28"
+                width={990}
+                height={650}
+              />{" "}
+              <div className="flex -mt-28 -pt-28">
+                <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1 pl-16 pr-10 mx-16  -mt-20">
+                  Answer few <br /> questions
+                </div>
+                <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1 pl-16 pr-10 mx-16  -mt-20">
+                  Register using <br /> phone and OTP
+                </div>
+                <div className="flex-2 px-3 font-poppins text-2xl font-semibold leading-tight tracking-er text-rgba-255-255-255-1 pr-10 mx-16  -mt-20">
+                  Get report and <br /> personal roadmap
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex pt-10 -pb-16 justify-center items-center">
+          <button className="bg-rgba-251-115-6-1  text-2xl hover:bg-rgba-251-115-6-8 text-rgba-255-255-255-1 font-bold  py-2 px-4 rounded-full shadow-xl">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      margin: ["responsive", "negative"],
+      padding: ["responsive", "negative"],
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "rgba-10-87-131-1": "rgba(10, 87, 131, 1)",
+        "rgba-24-161-154-1": "rgba(24, 161, 154, 1)",
+        "rgba-255-255-255-1": "rgba(255, 255, 255, 1)",
+        "rgba-255-255-255-5": "rgba(255, 255, 255, 0.7)",
+        "rgba-251-115-6-1": "rgba(251, 115, 6, 1)",
+        "rgba-251-115-6-8": "rgba(251, 115, 6, 0.9)",
       },
     },
   },
   plugins: [],
-}
+};
