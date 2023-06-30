768265eafaab5006fa81204d53f23b834d01188c
ayasahmad63 <ayasahmad63@gmail.com>
2023-06-30 20:23:08 +0530

    Ui Updtaed

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-sc    bg-gradient-to-r  from-rgba-10-87-131-1  to-rgba-24-161-154-1">
       <div className="h-2/3 flex justify-center items-center w-full  ">
-        <div className="w-2/3 p-3  pt-16 mt-4   text-center">
+        <div className="w-2/3 p-3  pt-28 mt-4   text-center">
           <h1 className="font-poppins text-6xl font-semibold leading-tight tracking-wider text-rgba-255-255-255-1">
             Check your financial health
           </h1>
@@ -20,9 +20,9 @@ export default function Home() {
           Get Started
         </button>
       </div>
-      <div className="flex pt-10">
-        <div className="ml-32 pl-28 pt-10 ">
-          <div className="p1-1 flex ml-16 pl-16 pt-14">
+      <div className="md:flex-row  justify-center items-center md:flex-no-wrap lg:flex pt-10">
+        <div className=" lg:pl-1 lg:pt-10 ">
+          <div className="p1-1 flex  lg:ml-16 pl-16 pt-14">
             <div className="flex">
               <Image
                 src="/tick.svg"
@@ -36,7 +36,7 @@ export default function Home() {
               Expected Retirement Age
             </div>
           </div>
-          <div className="pl-16 ml-16">
+          <div className="pl-16 lg:ml-16">
             <Image
               src="/Line.svg"
               alt="My Image"
@@ -45,7 +45,8 @@ export default function Home() {
               height={650}
             />
           </div>
-          <div className="pl-1 flex ml-16 pl-16 pt-4">
+
+          <div className="pl-1 flex lg:ml-16 pl-16 pt-4">
             <div className="flex">
               <Image
                 src="/tick.svg"
@@ -59,7 +60,7 @@ export default function Home() {
               Identify Mistake
             </div>
           </div>
-          <div className="pl-16 ml-16">
+          <div className="pl-16 lg:ml-16">
             <Image
               src="/Line.svg"
               alt="My Image"
@@ -70,25 +71,26 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex-1 -mr-28 -mb-28 -pr-28 g-green-500">
+        <div className="flex  -mr-28 -mb-28 lg:-pr-28 g-green-500">
           <div className="flex  justify-center items-center">
             <div className="justify-center items-center">
               <Image
                 src="/mobi.svg"
                 alt="My Image"
-                // className="-m-8 -mr-20"
+                className="lg:pl-16 lg:-pr-20"
                 width={480}
                 height={1500}
               />
             </div>
           </div>{" "}
         </div>
-        <div className="mr-32 pr-28 pt-10 z-10">
-          <div className="flex mr-1 px-1 pt-14 mr-16 pr-16">
+        <div className="   g:mr-32 lg:pr-8 lg:pl-8  lg:pt-3 pt-28">
+          <div className="flex mr-1 px-1 pt-14 lg:mr-8 lg:pr-8">
             <div className="flex ">
               <Image
                 src="/tick.svg"
                 alt="My Image"
+                t
                 className=""
                 width={27}
                 height={10}
@@ -98,13 +100,13 @@ export default function Home() {
               Personalised Road Map
             </div>
           </div>
-          <div className="px-1 ml-1">
+          <div className="px-1 ml-1 mr-16 pr-16">
             <Image
               src="/Line.svg"
               alt="My Image"
               className="py-3 ml-9"
               width={290}
-              height={950}
+              height={650}
             />
           </div>
           <div className="px-1 flex ml-1 px-1 pt-4">
@@ -132,6 +134,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* dfad */}
       <div
         className=" bg-rd-500 relative bg-cover bg-center bg-no-repeat  -mt-20 -pt-22 absolut  h-screen bg-cover bg-center bg-gradient-to-r  from-rgba-10-87-131-1  to-rgba-24-161-154-1 "
         style={{ backgroundImage: "url('/Vector1.svg')" }}
